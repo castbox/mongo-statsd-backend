1923f8438836e7687727999cad134802afeecd62
tianyi <shty1990@163.com>
2019-03-19 14:00:58 +0800

    fix

--- tests/index.js
+++ tests/index.js
@@ -1,43 +1,42 @@
 'use strict';
 
 var backend = require('../lib/'),
-	events = require('events'),
-	flush = require('./exampleFlush.js'),
-	assert = require('chai').assert;
+  events = require('events'),
+  flush = require('./exampleFlush.js'),
+  assert = require('chai').assert;
 
 var eventer = new events.EventEmitter(),
-	config = {
-		mongoHost: '127.0.0.1',
-		flushInterval: 5000,
-		debug: true
-	},
-	backendRef;
+  config = {
+    mongoHost: '127.0.0.1',
+    flushInterval: 5000,
+    debug: true
+  },
+  backendRef;
 
-describe('the mongodb statsd backend', function(){
+describe('the mongodb statsd backend', function() {
+  describe('verifying some deps', function() {
+    it('check existance of stubbed dependencies', function(d) {
+      assert.ok(eventer);
+      assert.ok(flush);
+      d();
+    });
+  });
 
-	describe('verifying some deps', function(){
-		it('check existance of stubbed dependencies', function(d){
-			assert.ok(eventer);
-			assert.ok(flush);
-			d();
-		});
-	});
+  describe('backend#init()', function() {
+    it('initializes the backend', function(d) {
+      backendRef = backend.init(new Date().getTime(), config, eventer);
+      assert.ok(backendRef);
+      d();
+    });
+  });
 
-	describe('backend#init()', function(){
-		it('initializes the backend', function(d){
-			backendRef = backend.init(new Date().getTime(), config, eventer);
-			assert.ok(backendRef);
-			d();
-		});
-	});
-
-	describe('eventer#emit("flush")', function(){
-		it('emits a flush event to the backend', function(d){
-			eventer.emit('flush', new Date().getTime(), flush);
-			setTimeout(function(){
-				assert.ok(true);
-				d();
-			}, 1500);
-		});
-	});
+  describe('eventer#emit("flush")', function() {
+    it('emits a flush event to the backend', function(d) {
+      eventer.emit('flush', new Date().getTime(), flush);
+      setTimeout(function() {
+        assert.ok(true);
+        d();
+      }, 1500);
+    });
+  });
 });
